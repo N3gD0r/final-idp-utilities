5c69fc999b7eec1d1aa21c81229268ea7dc0cb5d
David Hernandez Morones <david_hernandez1@epam.com>
2024-02-27 19:14:57 -0600

    Added utils module to calculate subnets

---
+++ src/utilities_david_hdez/network_calculation.py
@@ -0,0 +1,16 @@
+import ipaddress
+
+
+def available_subnets(cidr_block: str,
+                      public_subnets_count: int,
+                      private_subnets_count: int) -> (list, list):
+    network = ipaddress.ip_network(cidr_block)
+
+    subnet_mask = network.prefixlen + max(public_subnets_count, private_subnets_count)
+
+    total_cidr_blocks = list(network.subnets(new_prefix=subnet_mask))
+
+    public_subnets = total_cidr_blocks[:public_subnets_count]
+    private_subnets = total_cidr_blocks[public_subnets:public_subnets_count + private_subnets_count]
+
+    return public_subnets, private_subnets
